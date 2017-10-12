03d8e02df95a0e0656eaf319151ec2f5ee31478a
James Mackenzie <hunter24957@gmail.com>
2017-10-12 23:57:51 +0100

    Updated files

--- quantum.py
+++ quantum.py
@@ -1,10 +1,11 @@
 import xbmcaddon
 import xbmcgui
+import requests
  
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello World!"
+line1 = "Hello world"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
  
